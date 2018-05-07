d9f7efb651b812bb7bbc42a6609321f3bf098055
Ketan Bhadke <ketanbhadke@gmail.com>
2018-05-08 01:24:19 +0530

    Added sample fileReader and form structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactFileReader from 'react-file-reader';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  handleFiles = files => {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+    // Use reader.result
+    alert(reader.result)
+    }
+  reader.readAsText(files[0]);
+}
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      // returns only one div (can be nested though)!
+      <div>
+        <ReactFileReader handleFiles={this.handleFiles} fileTypes={'.txt'}>
+    <button className='btn'>Upload</button>
+</ReactFileReader>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange.bind(this)} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       </div>
     );
   }
